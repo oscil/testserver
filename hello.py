c5b385978aecdd01438af16437055299947907fe
root <admin@example.com>
2018-06-03 18:42:13 +0300

    dolgno rabotat

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def hello(environ, start_response):
+def application(environ, start_response):
 	out = "\n".join(environ.get('QUERY_STRING').split("&"))
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	return [out] 
--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart -c /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
