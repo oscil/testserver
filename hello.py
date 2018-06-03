faf3eb88e109c9209982003dd9b511cf2fc51d94
root <admin@example.com>
2018-06-03 17:28:45 +0300

    worker golij add python file

---
+++ hello.py
@@ -0,0 +1,4 @@
+def simplest_wsgi_app(environ, start_response):
+	out = "\n".join(environ.get('QUERY_STRING').split("&"))
+	start_response('200 OK', [('Content-Type', 'text/plain')])
+	return [out] 
